a9e076e56851d9b4b61e67dfab73d6edce6cbe3d
Mishel Tyurkova <misheltyurkova@gmail.com>
2023-05-16 11:41:23 +1000

    commit tree f04928d1e360667297b42d53ab94b16765b7db8e

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+
+destructivelyAppendCat('Ralph');
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name)
+}
+
+destructivelyPrependCat("Bob")
+
+function destructivelyRemoveLastCat() {
+    return cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift()
+}
+
+function appendCat(name) {
+    const copyOfCats = [...cats, name];
+    return copyOfCats
+}
+
+function prependCat(name) {
+    const copyOfCats = [name, ...cats];
+    return copyOfCats
+}
+
+function removeLastCat() {
+    return cats.slice(0, cats.length - 1)
+}
+
+function  removeFirstCat() {
+    return cats.slice(1)
+}
+
+
+
+
+
